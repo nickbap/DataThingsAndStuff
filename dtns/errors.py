9b3a3771d10e236930c4d291bdcad09c664fb334
Nick Bautista <nbautista12@gmail.com>
2021-12-18 17:56:39 -0800

    Add handler for 500

--- dtns/errors.py
+++ dtns/errors.py
@@ -12,3 +12,8 @@ def unauthorized(error):
 @error.app_errorhandler(404)
 def page_not_found(error):
     return render_template("errors/404.html"), 404
+
+
+@error.app_errorhandler(500)
+def internal_service_error(error):
+    return render_template("errors/500.html"), 500
