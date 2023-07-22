645429fae5b6d316437dd6fb836b565940e2c87f
Nick Bautista <nbautista12@gmail.com>
2023-07-21 18:42:58 -0700

    Add rendering for Pinterest pins or boards

--- dtns/utils/render_utils.py
+++ dtns/utils/render_utils.py
@@ -14,7 +14,7 @@ def render_blank_link(self, tokens, idx, options, env):
     return self.renderToken(tokens, idx, options, env)
 
 
-def render_youtube(self, tokens, idx, options, env):
+def render_youtube_pin_or_board(self, tokens, idx, options, env):
     """
     Render image syntax with youtube urls as embedded youtube player
 
@@ -39,9 +39,37 @@ def render_youtube(self, tokens, idx, options, env):
             + ' encrypted-media; gyroscope; picture-in-picture"\n allowfullscreen></iframe>\n'
             + "</div>"
         )
+
+    elif "https://www.pinterest.com/pin/" in token.attrs["src"]:
+
+        results = urlparse(token.attrs["src"])
+        if results:
+            path = results.path
+
+        return (
+            '<div class="d-flex justify-content-center">'
+            + "<div>"
+            + f'<a href="https://www.pinterest.com{path}" data-pin-do="embedPin"></a>'
+            + "</div>"
+            + "</div>"
+        )
+
+    elif "https://www.pinterest.com/" in token.attrs["src"]:
+
+        results = urlparse(token.attrs["src"])
+        if results:
+            path = results.path
+
+        return (
+            '<div class="d-flex justify-content-center">'
+            + '<a data-pin-do="embedBoard" data-pin-board-width="400" data-pin-scale-height="240"'
+            + f'data-pin-scale-width="80" href="https://www.pinterest.com{path}"></a>'
+            + "</div>"
+        )
+
     return self.image(tokens, idx, options, env)
 
 
 md = MarkdownIt("commonmark").enable("strikethrough")
-md.add_render_rule("image", render_youtube)
+md.add_render_rule("image", render_youtube_pin_or_board)
 md.add_render_rule("link_open", render_blank_link)
