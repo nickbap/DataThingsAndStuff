de7e7a3de3527584c69e66157aa0ff40ef5aad88
Nick Bautista <nbautista12@gmail.com>
2024-02-17 15:50:18 -0800

    Remove post_list_template for post-list.html

--- dtns/ajax.py
+++ dtns/ajax.py
@@ -4,6 +4,7 @@ from urllib.parse import unquote
 from flask import Blueprint
 from flask import current_app
 from flask import render_template_string
+from flask import render_template
 from flask import request
 from flask_login.utils import login_required
 
@@ -18,16 +19,16 @@ ajax = Blueprint("ajax", __name__)
 def search_posts():
     search_terms = request.form["search"]
     posts = PostModelStorage.search_posts(search_terms)
-    return render_template_string(
-        post_utils.post_list_template, criteria=search_terms, posts=posts
+    return render_template(
+        "components/post-list.html", criteria=search_terms, posts=posts
     )
 
 
 @ajax.route("/posts/<month_year>")
 def posts_month_year(month_year):
     posts = PostModelStorage.get_posts_by_month_year(unquote(month_year))
-    return render_template_string(
-        post_utils.post_list_template, criteria=month_year, posts=posts
+    return render_template(
+        "components/post-list.html", criteria=month_year, posts=posts
     )
 
 
