0e36213d8c3b360a245ee59dd235f699803d9cf7
Nick Bautista <nbautista12@gmail.com>
2024-02-25 17:10:39 -0800

    Remove test from email subject

--- dtns/utils/email_utils.py
+++ dtns/utils/email_utils.py
@@ -29,7 +29,7 @@ def send_email(subject, sender, recipients, msg_body=None, msg_html=None):
 
 def send_new_comment_notif(comment):
     send_email(
-        f"Test - new comment from: {comment.user.username}",
+        f"New Comment from: {comment.user.username}",
         ("Data Things and Stuff", current_app.config["MAIL_USERNAME"]),
         [current_app.config["SITE_ADMIN"]],
         None,
